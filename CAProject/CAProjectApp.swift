1a0b278c1ac3df8206b3ed9b1e8ae51bd86e77c6
Florencio Gallegos <fgalle34362007@yahoo.com>
2024-08-26 13:26:41 -0400

    Initial Commit

---
+++ CAProject/CAProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAProjectApp.swift
+//  CAProject
+//
+//  Created by Florencio Gallegos on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CAProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CAProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CAProject
+//
+//  Created by Florencio Gallegos on 8/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
